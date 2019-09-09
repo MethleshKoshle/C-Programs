a0c79534b0d4894e2fc3991a2e07620bd2a38446
MethleshKoshle <45479114+MethleshKoshle@users.noreply.github.com>
2019-09-09 19:40:14 +0530

    Create Int_array.c

---
+++ Int_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    char alpha[10];
+    int x, y;
+    const int size=10;
+    int name_arr[size];
+    for(int i=0; i<10; i++)
+    {
+        name_arr[i]=i*2;
+    }
+    printf("My array elements are: ");
+    for(int i=0; i<10; i++)
+    {
+        printf("%d ", name_arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
