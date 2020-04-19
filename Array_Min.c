cf53b070cb82beeebe415af5e02bd713dcefb76b
MethleshKoshle <45479114+MethleshKoshle@users.noreply.github.com>
2020-04-19 15:49:26 +0530

    Update Array_Min.c

--- Array_Min.c
+++ Array_Min.c
@@ -1,20 +1,24 @@
+/* Problem: Given an array by the user find the minimum of this array.*/
 #include <stdio.h>
-#define len 5
-
 main(){
-	int i=1;
-	float a[len];
+	int i=1, len;
+	printf("Enter the size of the array: ");
+	scanf("%d", len);
+	int a[len];
 
 	for(i=0; i<len; i++){
 		printf("a[%d]: ", i);
-		scanf("%f", &a[i]);
+		scanf("%d", &a[i]);
 	}
 
-	int min=a[0];
+	int min=a[0], id=0;
 
-	for(i=1; i<len; i++)
-		if(min>a[i])
+	for(i=1; i<len; i++){
+		if(min>a[i]){
 			min = a[i];
+			id=i;
+		}
+	}
 
-	printf("%dth element is minimum i.e. %d\n", i-1, min);
+	printf("%dth element is minimum i.e. %d\n", id, min);
 }
