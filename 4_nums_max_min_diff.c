d5ca2cfada3d352163da9f341c5e92024fe9794e
MethleshKoshle <45479114+MethleshKoshle@users.noreply.github.com>
2020-04-19 15:42:05 +0530

    Update 4_nums_max_min_diff.c

--- 4_nums_max_min_diff.c
+++ 4_nums_max_min_diff.c
@@ -1,8 +1,11 @@
+/* Problem: Given four real numbers from user find difference between maximum and minimum of these numbers.
+   answer=max(a, b, c, d)-min(a, b, c, d)
+*/
 #include <stdio.h>
 int main(){
-	float a, b, c, d, max, min;
-	printf("Input four numbers: ");
-	scanf("%f %f %f %f", &a, &b, &c, &d);
+    float a, b, c, d, max, min;
+    printf("Input four numbers: ");
+    scanf("%f %f %f %f", &a, &b, &c, &d);
 
     max = a;
     min = a;
@@ -27,7 +30,7 @@ int main(){
     	max = d;
     if(min>=d)
     	min = d;
-
+    // printing the answer upto 4 digits after decimal
     printf("Difference is: %.4f\n", max-min);
 	return 0;
 }
