9204740d348c94df390e8e8f492f310559db1150
MethleshKoshle <45479114+MethleshKoshle@users.noreply.github.com>
2020-04-19 15:47:06 +0530

    Update Array_Max.c

--- Array_Max.c
+++ Array_Max.c
@@ -1,19 +1,24 @@
+/* Problem: Given an array by the user find the maximum of this array.*/
 #include <stdio.h>
-#define len 5
 main(){
-	int i=1;
-	float a[len];
+	int i=1, len;
+	printf("Enter the size of the array: ");
+	scanf("%d", len);
+	int a[len];
 
 	for(i=0; i<len; i++){
 		printf("a[%d]: ", i);
-		scanf("%f", &a[i]);
+		scanf("%d", &a[i]);
 	}
 
-	int max=a[0];
+	int max=a[0], id=0;
 
-	for(i=1; i<len; i++)
-		if(max<a[i])
+	for(i=1; i<len; i++){
+		if(max<a[i]){
 			max = a[i];
+			id=i;
+		}
+	}
 
-	printf("%dth element is maximum i.e. %d\n", i-1, max);
+	printf("%dth element is maximum i.e. %d\n", id, max);
 }
